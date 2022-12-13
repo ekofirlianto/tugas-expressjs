b41a426581a7a39134fa11b4b4f63175d54eda85
Eko Firlianto <eko.frilianto@gmail.com>
2022-12-13 19:03:07 +0700

    menambahkan file materi dan tugas

---
+++ tugas.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!<h1>');
+});
+
+app.get('/about', (req, res) => {
+  res.send('<h1>ini halaman about<h1>');
+});
+
+app.get('/myself', (req, res) => {
+  res.json({
+    nama: 'Eko Firlianto',
+    kelas: 'MERN Stack',
+    instansi: 'Eduwork',
+  });
+});
+
+app.use('/', (req, res) => {
+  res.status(404);
+  res.send('<h1>Page not found 404<h1>');
+});
+
+app.listen(process.env.PORT || 3000);
